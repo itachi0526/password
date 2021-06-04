fda2cafea4b211c7052e0ee13d21973bfa6eaf5a
Eric Haung <fiser0317@gmail.com>
2021-06-04 11:49:39 +0800

    second version

--- password.py
+++ password.py
@@ -5,7 +5,8 @@ while x > 0:
         print('麥當勞歡樂送！！！')
         break
     else:
-        x = x - 1
-        print('笑死，熊頭你只剩下', x,'次機會')
-        if x == 0: #
-            break           
+        x = x - 1 # 也可放while後面
+        if x > 0: #
+            print('笑死，熊頭你只剩下', x,'次機會')
+        else: #
+            print('沒機會了，開門！')               
